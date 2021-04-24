f96013a4c86881d49a20dffc4c3f1380424bf5a8
Matheus Gonzaga <matheus.gonzaga@marketup.com>
2021-04-24 19:28:09 -0300

    api kotlin

---
+++ src/main/kotlin/com/example/bankapi/repository/AccountRepository.kt
@@ -0,0 +1,7 @@
+package com.example.bankapi.repository
+
+import com.example.bankapi.model.Account
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AccountRepository: JpaRepository<Account, Long> {
+}
